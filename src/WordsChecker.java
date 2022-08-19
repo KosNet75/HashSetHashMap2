0ec25520df1a7c4e3129f18dfd075a160dffbf33
KosNet75 <nemokonstantin@gmail.com>
2022-08-19 13:42:17 +0700

    3 commit

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class WordsChecker {
   static String text;
