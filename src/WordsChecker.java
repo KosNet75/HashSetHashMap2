eab83c3a99fbef22f0dc5280497237081d86270e
KosNet75 <nemokonstantin@gmail.com>
2022-08-19 13:33:29 +0700

    3 commit

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WordsChecker {
   static String text;
@@ -10,16 +12,31 @@ public class WordsChecker {
   }
 
   public static void hasWord(String word) {
+    List<String> set = new ArrayList<>();
 
-    Set<String> set = new HashSet<>();
+//    Set<String> set = new HashSet<>();
 
     Collections.addAll(set, text.split("\\P{IsAlphabetic}+"));
-//    for (String s : text.split("\\P{IsAlphabetic}+")) {
-//      set.add(s);
+////    for (String s : text.split("\\P{IsAlphabetic}+")) {
+////      set.add(s);
     System.out.println();
     System.out.println(set);
     System.out.println(
         "\nПоиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(word) + "]");
+
+
+
+
+
+//   // Collections.addAll(set, text.split("\\P{IsAlphabetic}+"));
+//
+//    List<String> result = set.stream()
+//        .filter(lang -> lang.contains(word)).toList();
+//    if (!result.isEmpty()) {System.out.println("Слово [" + word + "] найдено в этом тексте");}
+//    else System.out.println("Такого слова нет в тексте");
+//
+//
+
   }
 }
 
