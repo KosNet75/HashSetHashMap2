25eda8a5a5e8aba6ef08010efee0cf62f162706b
KosNet75 <nemokonstantin@gmail.com>
2022-08-20 10:36:41 +0700

    4 commit

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -11,7 +11,6 @@ public class WordsChecker {
   }
 
   public static void hasWord(String word) {
-    // List<String> set = new ArrayList<>();
 
     Set<String> set = new HashSet<>();
 
@@ -21,26 +20,16 @@ public class WordsChecker {
     System.out.println();
     System.out.println(set);
     System.out.println(
-        "\nПоиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(word) + "]");
+        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(word) + "]");
 
 
 ///без contains..
 
     if(!set.add(word)) {
-      System.out.println("В этом тексте найдено слово " + word);
-    }else System.out.println("В этом тексте не найдено слово "  + word);
+      System.out.println("без contains:   В этом тексте найдено слово " + word);
+    }else System.out.println("без contains:   В этом тексте не найдено слово "  + word);
     set.remove(word);
 
-
-
-//
-//    int indexJava = set.indexOf(word);
-//    System.out.println(" вариант без contains..????");
-//    if(indexJava == - 1) {
-//      System.out.println("Слово \"voluptate\" не найдено.");
-//    } else {
-//      System.out.println("Слово \"" + word + "\" найдено в тексте");
-//     }
 
   }
 }
