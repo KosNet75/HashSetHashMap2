970c4638fe7b39f85799598aa3e4785edb8a1465
KosNet75 <nemokonstantin@gmail.com>
2022-08-21 18:44:19 +0700

    5 commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
     WordsChecker wordsChecker = new WordsChecker(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
@@ -9,7 +12,11 @@ public class Main {
             + "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
             + "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
-    WordsChecker.hasWord("voluptate");
 
+    System.out.print("Введите слово для поиска: > ");
+    String input = scanner.nextLine();
+    if(WordsChecker.hasWord(input)) {
+      System.out.println("без contains:   В этом тексте найдено слово " + input);
+    }else System.out.println("без contains:   В этом тексте не найдено слово " + input);
   }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,34 +1,25 @@
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 
 public class WordsChecker {
+
+  static Set<String> set = new HashSet<>();
   static String text;
 
   public WordsChecker(String text) {
+
     WordsChecker.text = text;
-  }
-
-  public static void hasWord(String word) {
-
-    Set<String> set = new HashSet<>();
-
 
     Collections.addAll(set, text.split("\\P{IsAlphabetic}+"));
+  }
 
-    System.out.println();
-    System.out.println(set);
-    System.out.println(
-        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(word) + "]");
+  public static boolean hasWord(String word) {
+//    System.out.println();
+//    System.out.println(set);
 
-
-    if(!set.add(word)) {
-      System.out.println("без contains:   В этом тексте найдено слово " + word);
-    }else System.out.println("без contains:   В этом тексте не найдено слово "  + word);
-    set.remove(word);
-
-
+    return !set.add(word);
   }
 }
-
