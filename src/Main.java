f994268a658c4bcc4bff036d92aba95cf64df29b
KosNet75 <nemokonstantin@gmail.com>
2022-08-22 21:29:05 +0700

    5 commit

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
 
-    WordsChecker wordsChecker = new WordsChecker(
+
+    String textIn =
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
             + " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, "
             + "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure "
             + "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
-            + "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            + "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+    WordsChecker text = new WordsChecker(textIn);
 
-    WordsChecker.hasWord("reprehenderit");
+    String word = "ullamco";
+    System.out.print("[" + word + "] в этом тексте - " + text.hasWord(word));
 
   }
 }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,36 +1,17 @@
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class WordsChecker {
-
-  static Set<String> set = new HashSet<>();
-  static String text;
-
+  Set<String> set = new HashSet<>();
+  protected String text;
   public WordsChecker(String text) {
-
-    WordsChecker.text = text;
+    this.text = text;
 
     Collections.addAll(set, text.split("\\P{IsAlphabetic}+"));
   }
 
-  public static boolean hasWord(String word) {
-
-    System.out.println(set);
-    System.out.println(
-        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(
-            word) + "]");
-    System.out.println();
-
-    if (!set.add(word)) {
-      System.out.println("В этом тексте найдено слово " + word);
-      return true;
-    } else {
-      System.out.println("В этом тексте не найдено слово " + word);
-      set.remove(word);
-      return false;
-    }
+  public boolean hasWord(String word) {
+    return text.contains(word);
   }
 }
