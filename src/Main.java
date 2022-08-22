490f38a21035eebfad3dcb78264711335fede0c9
KosNet75 <nemokonstantin@gmail.com>
2022-08-22 19:01:17 +0700

    5 commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             + "dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
             + "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
+
     WordsChecker.hasWord("reprehenderit");
 
   }
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -18,18 +18,18 @@ public class WordsChecker {
 
   public static boolean hasWord(String word) {
 
-    System.out.println(set);
-    System.out.println(
-        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(
-            word) + "]");
-    System.out.println();
+//    System.out.println(set);
+//    System.out.println(
+//        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(
+//            word) + "]");
+//    System.out.println();
 
     if (!set.add(word)) {
       System.out.println("В этом тексте найдено слово " + word);
-      set.remove(word);
       return true;
     } else {
       System.out.println("В этом тексте не найдено слово " + word);
+      set.remove(word);
       return false;
     }
   }
