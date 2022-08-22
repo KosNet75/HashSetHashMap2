3a28e9510b5c87c0fa031aee013357d2d2a50eb8
KosNet75 <nemokonstantin@gmail.com>
2022-08-22 15:17:06 +0700

    5 commit

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -15,14 +15,18 @@ public class WordsChecker {
 
     Collections.addAll(set, text.split("\\P{IsAlphabetic}+"));
   }
-
   public static boolean hasWord(String word) {
-    if (!set.add(word)) {
-      System.out.println("В этом тексте найдено слово " + word);
-      return true;
-    } else {
-      System.out.println("В этом тексте не найдено слово " + word);
-      return false;
+
+    System.out.println();
+    System.out.println(set);
+    System.out.println(
+        "\ncontains:   Поиск в тексте слова: [" + word + "] результат поиска - [" + text.contains(word) + "]");
+
+//    if (!set.add(word)) {
+//      System.out.println("В этом тексте найдено слово " + word);
+//      return true;
+//    } else {
+//      System.out.println("В этом тексте не найдено слово " + word);
+     return false;
     }
-  }
 }
